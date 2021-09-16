e7457b38bbd7019576b18fa8746ddbba266a7b4c
prateekmadhesyia <prateek.madhesiya.17@mountblue.tech>
2021-09-16 14:39:37 +0530

    Added method

--- index.js
+++ index.js
@@ -2,83 +2,130 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
-const port = 8000;
+const port = 8080;
+
+function readFile(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
 
 http
   .createServer((request, response) => {
     let url = request.url;
+    let method = request.method;
     let urlArray = url.split("/");
     let value = urlArray[urlArray.length - 1];
 
     if (url == "/") {
-      response.writeHead(200, "OK", { "Content-Type": "text/html" });
-      response.write("<h1> Welcome to home page </h1>");
-      response.end();
+      if (method == "GET") {
+        response.writeHead(200, "OK", { "Content-Type": "text/html" });
+        response.write("<h1> Welcome to home page </h1>");
+        response.end();
+      } else {
+        response.writeHead(501, "Not Implemented");
+        response.write("<h1> 501, Not Implemented</h1>");
+        response.end();
+      }
     } else if (url == "/html") {
-      fs.readFile(path.join(__dirname, "quote.html"), "utf8", (err, data) => {
-        if (err) {
-          console.error(err);
-          response.writeHead(503, "Service Unavailable");
-          response.write("Retry after sometime");
-          response.end();
-        } else {
-          response.writeHead(200, "OK", { "Content-Type": "text/html" });
-          response.write(data);
-          response.end();
-        }
-      });
-    } else if (url == "/json") {
-      fs.readFile(
-        path.join(__dirname, "secondJson.json"),
-        "utf8",
-        (err, data) => {
-          if (err) {
+      if (method == "GET") {
+        readFile(path.join(__dirname, "quote.html"))
+          .then((data) => {
+            response.writeHead(200, "OK", { "Content-Type": "text/html" });
+            response.write(data);
+            response.end();
+          })
+          .catch((err) => {
             console.error(err);
             response.writeHead(503, "Service Unavailable");
             response.write("Retry after sometime");
             response.end();
-          } else {
+          });
+      } else {
+        response.writeHead(501, "Not Implemented");
+        response.write("<h1> 501, Not Implemented</h1>");
+        response.end();
+      }
+    } else if (url == "/json") {
+      if (method == "GET") {
+        readFile(path.join(__dirname, "secondJson.json"))
+          .then((data) => {
             response.writeHead(200, "OK", {
               "Content-Type": "application/json",
             });
             response.write(data);
             response.end();
-          }
-        }
-      );
+          })
+          .catch((err) => {
+            console.error(err);
+            response.writeHead(503, "Service Unavailable");
+            response.write("Retry after sometime");
+            response.end();
+          });
+      } else {
+        response.writeHead(501, "Not Implemented");
+        response.write("<h1> 501, Not Implemented</h1>");
+        response.end();
+      }
     } else if (url == "/uuid") {
-      let newId = uuidv4();
-      let data = { uuid: newId };
+      if (method == "GET") {
+        let newId = uuidv4();
+        let data = { uuid: newId };
 
-      response.writeHead(200, "OK", { "Content-Type": "application/json" });
-      response.write(JSON.stringify(data));
-      response.end();
-    } else if (url == `/status/${value}`) {
-      let allCode = http.STATUS_CODES;
-
-      if (allCode[value] != undefined) {
-        // response.writeHead(value, allCode[value], {
-        //   "Content-Type": "text/html",
-        // });
-
-        response.writeHead(200, "OK", {
-          "Content-Type": "text/html",
-        });
-        response.write(`<h1> Message-- ${allCode[value]}</h1>`);
+        response.writeHead(200, "OK", { "Content-Type": "application/json" });
+        response.write(JSON.stringify(data));
         response.end();
       } else {
-        response.writeHead(422, "Unprocessable entry", {
-          "Content-Type": "text/html",
-        });
-        response.write(`<h1> Invalid status code-- ${value} </h1>`);
+        response.writeHead(501, "Not Implemented");
+        response.write("<h1> 501, Not Implemented</h1>");
+        response.end();
+      }
+    } else if (url == `/status/${value}`) {
+      if (method == "GET") {
+        let allCode = http.STATUS_CODES;
+
+        if (allCode[value] != undefined) {
+          // response.writeHead(value, allCode[value], {
+          //   "Content-Type": "text/html",
+          // });
+
+          response.writeHead(200, "OK", {
+            "Content-Type": "text/html",
+          });
+          response.write(`<h1> Message-- ${allCode[value]}</h1>`);
+          response.end();
+        } else {
+          response.writeHead(422, "Unprocessable entry", {
+            "Content-Type": "text/html",
+          });
+          response.write(`<h1> Invalid status code-- ${value} </h1>`);
+          response.end();
+        }
+      } else {
+        response.writeHead(501, "Not Implemented");
+        response.write("<h1> 501, Not Implemented</h1>");
         response.end();
       }
     } else if (url == `/delay/${value}`) {
-      setTimeout(() => {
-        response.writeHead(200, "OK", { "Content-Type": "application/json" });
-        response.write(`<h1>You have been waiting for ${value} seconds.</h1>`);
+      if (method == "GET") {
+        setTimeout(() => {
+          response.writeHead(200, "OK", { "Content-Type": "text/html" });
+          response.write(
+            `<h1>You have been waiting for ${value} seconds.</h1>`
+          );
+          response.end();
+        }, value * 1000);
+      } else {
+        response.writeHead(501, "Not Implemented");
+        response.write("<h1> 501, Not Implemented</h1>");
         response.end();
-      }, value * 1000);
+      }
     } else {
       response.writeHead(404, "Not Found");
       response.write(
